8792ad9c4c5d8827fe7880c1da6f28b6106254cb
ksenialke <41840503+ksenialke@users.noreply.github.com>
2018-12-20 23:30:42 +0100

    Update numbers.js

--- numbers.js
+++ numbers.js
@@ -8,13 +8,14 @@ const numbers = {
     6:'六',
     7:'七',
     8:'八',
-    9:'九'
+    9:'九',
+    10:'十'
     // 2.2:'两'
 };
 
 const numberz = ['一','二','三','四','五','六','七','八','九'];
-const bigNumbers = ['十','百','千','万'];
-const nums = [10, 100, 1000, 10000];
+const bigNumbers = ['一','十','百','千','万'];
+const nums = [1, 10, 100, 1000, 10000];
 
 function convertNumbers(number) {
     if(number > 0 && number < 10){
@@ -28,7 +29,7 @@ function convertNumbers(number) {
 
         if (current >= value) {
             alreadyStarted = true;
-            toSave = Math.floor(current/i);
+            toSave = Math.floor(current/value);
             if (!(10 < current && current < 20)) {
                 out.push(convertNumbers(toSave));
             }
@@ -40,8 +41,8 @@ function convertNumbers(number) {
         else if (alreadyStarted) {
             out.push('零');
         }
-        return out;
+        return out.join('');
     }
 }
 
-console.log(convertNumbers(85));
+console.log(convertNumbers(175321));
