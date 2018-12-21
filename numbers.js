d0f9b64fb9f6bc94efdc74c283f6ed82575af489
ksenialke <41840503+ksenialke@users.noreply.github.com>
2018-12-21 18:49:42 +0100

    fixed algorithm

--- numbers.js
+++ numbers.js
@@ -1,48 +1,42 @@
-const numbers = {
-    0:'零',
-    1:'一',
-    2:'二',
-    3:'三',
-    4:'四',
-    5:'五',
-    6:'六',
-    7:'七',
-    8:'八',
-    9:'九',
-    10:'十'
-    // 2.2:'两'
-};
-
-const numberz = ['一','二','三','四','五','六','七','八','九'];
-const bigNumbers = ['一','十','百','千','万'];
-const nums = [1, 10, 100, 1000, 10000];
+const numbers = ['零','一','二','三','四','五','六','七','八','九','十'];
+const bigNumbers = ['亿', '万', '千', '百', '十', '一'];
+const nums = [ 100000000, 10000, 1000, 100, 10, 1];
+const liang = '两';
 
 function convertNumbers(number) {
-    if(number > 0 && number < 10){
-        return numberz[number-1]
+    if(number >= 0 && number <= 10){
+        return numbers[number]
     }
     let current = number;
     let out = [];
     let alreadyStarted = false;
 
-    for( let [i, value] of nums.reverse().entries()) {
-
+    for( let [i, value] of nums.entries()) {
         if (current >= value) {
             alreadyStarted = true;
-            toSave = Math.floor(current/value);
+            let toSave = Math.floor(current/value);
             if (!(10 < current && current < 20)) {
                 out.push(convertNumbers(toSave));
             }
             if (value !== 1) {
-                out.push(bigNumbers.reverse()[i]);
+                out.push(bigNumbers[i]);
             }
             current = current - (value*toSave);
         }
         else if (alreadyStarted) {
             out.push('零');
         }
-        return out.join('');
     }
+    out = out.join('').replace(/零+/, '零');
+
+    if ( out.includes('二千') || out.includes('二万') ) {
+        out = out.replace(numbers[2], liang)
+    }
+    if(out[out.length-1] === '零') {
+        return out.slice(0, -1);
+    }
+
+    return out
 }
 
-console.log(convertNumbers(175321));
+console.log(convertNumbers(652455));
