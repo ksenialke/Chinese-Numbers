8c6e7e5a065c36211ad141a6a94db3780379c509
ksenialke <41840503+ksenialke@users.noreply.github.com>
2018-12-22 18:26:24 +0100

    fixed algorithm

--- numbers.js
+++ numbers.js
@@ -1,9 +1,10 @@
 const numbers = ['零','一','二','三','四','五','六','七','八','九','十'];
 const bigNumbers = ['亿', '万', '千', '百', '十', '一'];
 const nums = [ 100000000, 10000, 1000, 100, 10, 1];
-const liang = '两';
+const submitButton = document.querySelector('button');
+const writeNumber = document.querySelector('#output');
 
-function convertNumbers(number) {
+function convertNumbersRecursive(number) {
     if(number >= 0 && number <= 10){
         return numbers[number]
     }
@@ -16,27 +17,34 @@ function convertNumbers(number) {
             alreadyStarted = true;
             let toSave = Math.floor(current/value);
             if (!(10 < current && current < 20)) {
-                out.push(convertNumbers(toSave));
+                out.push(convertNumbersRecursive(toSave));
             }
             if (value !== 1) {
                 out.push(bigNumbers[i]);
             }
-            current = current - (value*toSave);
+            current = current % value;
         }
         else if (alreadyStarted) {
             out.push('零');
         }
     }
-    out = out.join('').replace(/零+/, '零');
+    return out.join('')
+}
 
-    if ( out.includes('二千') || out.includes('二万') ) {
-        out = out.replace(numbers[2], liang)
-    }
+function convertNumbers(number)
+{
+    out = convertNumbersRecursive(number)
+    out = out.replace(/零+/, '零');
+    out = out.replace(/(^|[亿|万])二千/, '$1两千');
+    out = out.replace(/(^|亿)二万/, '$1两万');
+
     if(out[out.length-1] === '零') {
         return out.slice(0, -1);
     }
-
-    return out
+    return out;
 }
 
-console.log(convertNumbers(652455));
+submitButton.addEventListener('click', function () {
+    var toConvert = document.getElementById('arabicnumber').value;
+    writeNumber.textContent = convertNumbers(toConvert);
+});
